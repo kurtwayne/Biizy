854834382eae327eb19561dce7cbaf44f4d2d2a8
kurtwayne <kurtwgardner@gmail.com>
2017-04-21 10:14:28 -0600

    Add existing form data and mentor-instructed ctrl model setup

--- public/app/scripts/controllers/step1newCtrl.js
+++ public/app/scripts/controllers/step1newCtrl.js
@@ -1,11 +1,10 @@
 angular
     .module('biizyApp')
     .controller("step1newCtrl", function($rootScope, $scope, $state) {
+                    $scope.currentWizard = $rootScope.currentWizard;
                     $scope.next = function() {
-                        console.log("Step1 button Clicked")
                         $rootScope.currentWizard = {};
                         $rootScope.currentWizard.entity = {};
-                        $rootScope.currentWizard.newEntity = {};
                         $state.go("step2new");
                     }
     });
